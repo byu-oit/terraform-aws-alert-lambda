1f01dd1236900ef9a7eed52066c67de6e611c218
David Caldwell <dwclogic@gmail.com>
2022-10-17 14:40:24 -0600

    setting up terraform details

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ provider "aws" {
 
 locals {
   add_permission_boundary = length(var.lambda_role_permissions_boundary) > 0
+  filename                = "${path.module}/lambda/function.zip"
 }
 
 # IAM policies for the run lambda
@@ -37,10 +38,49 @@ resource "aws_iam_role" "lambda_role" {
 }
 
 # lambda
+resource "aws_lambda_function" "sns_to_operations" {
+  function_name    = "${var.app_name}-sns-to-operations"
+  filename         = local.filename
+  handler          = "index.handler"
+  runtime          = "nodejs14.x"
+  role             = aws_iam_role.lambda_role.arn
+  timeout          = var.timeout
+  memory_size      = var.memory_size
+  source_code_hash = filebase64sha256(local.filename)
+  tags             = var.tags
+
+  environment {
+    variables = {
+      APP_NAME = var.app_name
+      IN_DEV   = var.in_dev
+    }
+  }
+  depends_on = [aws_cloudwatch_log_group.logging]
+}
+
+resource "aws_cloudwatch_log_group" "logging" {
+  name              = "/aws/lambda/${var.app_name}-sns-to-operations"
+  retention_in_days = var.log_retention_in_days
+}
 
 # Lambda permission
+resource "aws_lambda_permission" "sns_to_operations" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.sns_to_operations.function_name
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.sns_to_operations.arn
+}
 
 # sns Alarm topic and subscription
+resource "aws_sns_topic" "sns_to_operations" {
+  name = "${var.app_name}-sns-to-operations"
+}
+
+resource "aws_sns_topic_subscription" "lambda" {
+  topic_arn = aws_sns_topic.sns_to_operations.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.sns_to_operations.arn
+}
 
 # Create array of cloud watch alarms
 
