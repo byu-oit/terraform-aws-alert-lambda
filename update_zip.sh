8ef7e956691246afda7c43cfeaa43cf770f657b5
David Caldwell <dwclogic@gmail.com>
2022-11-08 14:56:03 -0700

    2022-11-08 Alert object is good now, and implementing the push to Operations.  I tried https for simplicity first

---
+++ update_zip.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "updating the zip file with the current index.js"
+echo "cd ./lambda..."
+cd lambda
+echo "deleting the old zipfile..."
+rm function.zip
+echo "zipping new index.js"
+zip function.zip index.js
+cd ..
+echo "done..."
